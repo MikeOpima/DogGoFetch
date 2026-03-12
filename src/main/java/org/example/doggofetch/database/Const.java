82e6f8b74d48d3e202ebf0dab96221b24d5c5c84
Mike Opima <MO03@myscc.ca>
2026-03-12 10:22:11 -0400

    Removed the Const file

--- src/main/java/org/example/doggofetch/database/Const.java
+++
@@ -1,7 +0,0 @@
-package org.example.doggofetch.database;
-
-public class Const {
-    public static final String DB_NAME = "mopimajava";
-    public static final String DB_USER = "mopima";
-    public static final String DB_PASS = "p92vdp92vd";
-}
