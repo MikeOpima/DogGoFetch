f1a3da44f610e14db948200942eb8cb45c929b18
hc <hcharikhi@gmail.com>
2026-03-23 14:53:27 -0400

    Create Order DAO

--- src/main/java/org/example/doggofetch/dao/OrderDAO.java
+++ src/main/java/org/example/doggofetch/dao/OrderDAO.java
@@ -1,4 +1,11 @@
 package org.example.doggofetch.dao;
 
+import org.example.doggofetch.pojo.Inventory;
+
+import java.util.ArrayList;
+
 public interface OrderDAO {
+
+    public ArrayList<Inventory> getAllOrders();
+    public Inventory getOrder(int id);
 }
