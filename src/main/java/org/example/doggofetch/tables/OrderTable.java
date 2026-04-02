9d89ffcba1b5c4cdb57e29268766032fcdc1693f
hc <hcharikhi@gmail.com>
2026-04-02 19:17:21 -0400

    Add CRUD methods to OrderTable * Update SELECT * query not to create table * Update getAllOrders() * Update getOrder(int id)

--- src/main/java/org/example/doggofetch/tables/OrderTable.java
+++ src/main/java/org/example/doggofetch/tables/OrderTable.java
@@ -14,7 +14,7 @@ public class OrderTable implements OrderDAO {
     ArrayList<Order> order;
     @Override
     public ArrayList<Order> getAllOrders() {
-        String query = "SELECT * FROM " + DBConst.CREATE_TABLE_ORDER;
+        String query = "SELECT * FROM " + DBConst.TABLE_ORDER;
         order = new ArrayList<>();
         try{
             Statement getOrder = db.getConnection().createStatement();
@@ -24,7 +24,8 @@ public class OrderTable implements OrderDAO {
                         data.getInt(DBConst.ORDER_COLUMN_ID),
                         data.getString(DBConst.ORDER_COLUMN_DATE),
                         data.getInt(DBConst.ORDER_COLUMN_QUANTITY),
-                        data.getString(DBConst.ORDER_COLUMN_STATUS)
+                        data.getString(DBConst.ORDER_COLUMN_STATUS),
+                        (Integer) data.getObject(DBConst.ORDER_COLUMN_USER_ID)
                 ));
             }
             return order;
@@ -46,7 +47,8 @@ public class OrderTable implements OrderDAO {
                         data.getInt(DBConst.ORDER_COLUMN_ID),
                         data.getString(DBConst.ORDER_COLUMN_DATE),
                         data.getInt(DBConst.ORDER_COLUMN_QUANTITY),
-                        data.getString(DBConst.ORDER_COLUMN_STATUS)
+                        data.getString(DBConst.ORDER_COLUMN_STATUS),
+                        (Integer) data.getObject(DBConst.ORDER_COLUMN_USER_ID)
                 );
             }
         }catch(Exception e){
@@ -55,5 +57,54 @@ public class OrderTable implements OrderDAO {
         return null;
     }
 
+    @Override
+    public void createOrder(Order orderObj) {
+        String query = "INSERT INTO " + DBConst.TABLE_ORDER +
+                "(" + DBConst.ORDER_COLUMN_DATE + ", " +
+                DBConst.ORDER_COLUMN_QUANTITY + ", " +
+                DBConst.ORDER_COLUMN_STATUS + ", " +
+                DBConst.ORDER_COLUMN_USER_ID + ") VALUES ('" +
+                orderObj.getOrderDate() + "', " +
+                orderObj.getOrderQuantity() + ", '" +
+                orderObj.getOrderStatus() + "', " +
+                (orderObj.getUserId() == null ? "NULL" : orderObj.getUserId()) + ")";
 
+        try {
+            db.getConnection().createStatement().execute(query);
+            System.out.println("Created Order");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateOrder(Order orderObj) {
+        String query = "UPDATE " + DBConst.TABLE_ORDER + " SET " +
+                DBConst.ORDER_COLUMN_DATE + " = '" + orderObj.getOrderDate() + "', " +
+                DBConst.ORDER_COLUMN_QUANTITY + " = " + orderObj.getOrderQuantity() + ", " +
+                DBConst.ORDER_COLUMN_STATUS + " = '" + orderObj.getOrderStatus() + "', " +
+                DBConst.ORDER_COLUMN_USER_ID + " = " +
+                (orderObj.getUserId() == null ? "NULL" : orderObj.getUserId()) + " " +
+                "WHERE " + DBConst.ORDER_COLUMN_ID + " = " + orderObj.getOrderId();
+
+        try {
+            db.getConnection().createStatement().executeUpdate(query);
+            System.out.println("Updated Order");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteOrder(int id) {
+        String query = "DELETE FROM " + DBConst.TABLE_ORDER +
+                " WHERE " + DBConst.ORDER_COLUMN_ID + " = " + id;
+
+        try {
+            db.getConnection().createStatement().execute(query);
+            System.out.println("Deleted Order");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
