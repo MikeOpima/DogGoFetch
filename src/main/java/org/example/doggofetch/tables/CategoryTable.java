426dbf9d0df69c3f52e42556d3b06f573b68230a
hc <hcharikhi@gmail.com>
2026-04-02 21:21:15 -0400

    Update CategoryTable methods: * getAllCategory() * getCategory(int id)

--- src/main/java/org/example/doggofetch/tables/CategoryTable.java
+++ src/main/java/org/example/doggofetch/tables/CategoryTable.java
@@ -1,7 +1,9 @@
 package org.example.doggofetch.tables;
 
+import org.example.doggofetch.database.DBConst;
 import org.example.doggofetch.database.Database;
 import org.example.doggofetch.dao.CategoryDAO;
+import org.example.doggofetch.pojo.Address;
 import org.example.doggofetch.pojo.Category;
 import org.example.doggofetch.pojo.Product;
 
@@ -14,6 +16,9 @@ import java.util.ArrayList;
 
 import static org.example.doggofetch.database.DBConst.*;
 
+/**
+ * Hania - updated getAllCategory() method
+ */
 
 public class CategoryTable implements CategoryDAO {
     private static CategoryTable instance;
@@ -21,10 +26,38 @@ public class CategoryTable implements CategoryDAO {
     ArrayList<Category> categories;
 
     public ArrayList<Category> getAllCategory() {
+        categories = new ArrayList<>();
+        String query = "SELECT * FROM " + TABLE_CATEGORY;
+        try {
+            PreparedStatement getCategories = db.getConnection().prepareStatement(query);
+            ResultSet data = getCategories.executeQuery();
+            while (data.next()) {
+                categories.add(new Category(
+                        data.getInt(CATEGORY_COLUMN_ID),
+                        data.getString(CATEGORY_COLUMN_NAME)
+                ));
+            }
+            return categories;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
-    } // end getALLCategory
+    }//end getAllCategory
 
     public Category getCategory(int id){
+        String query = "SELECT * FROM " + TABLE_CATEGORY +
+                " WHERE " + DBConst.CATEGORY_COLUMN_ID + " = " + id;
+        try {
+            Statement getCategory = db.getConnection().createStatement();
+            ResultSet data = getCategory.executeQuery(query);
+            if (data.next()) {
+                return new Category(
+                        data.getInt(DBConst.CATEGORY_COLUMN_ID),
+                        data.getString(CATEGORY_COLUMN_NAME) );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     } // end getCategory
 
