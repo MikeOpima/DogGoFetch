42e27d43b3ef76fb136516a0e71d732736377e97
Mike Opima <MO03@myscc.ca>
2026-04-13 21:38:15 -0400

    Adding removeSupplierTab and SupplierTab

--- src/main/java/org/example/doggofetch/tabs/supplier/SupplierTab.java
+++ src/main/java/org/example/doggofetch/tabs/supplier/SupplierTab.java
@@ -1,10 +1,11 @@
+
 /**
  * Supplier Tab
  * Mike - April 9
  * Tab to display supplier info
  */
 
-        package org.example.doggofetch.tabs.supplier;
+package org.example.doggofetch.tabs.supplier;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.Tab;
@@ -28,57 +29,57 @@ public class SupplierTab extends Tab {
         TableColumn<Supplier, String> getSupplierStringTableColumn;
 
         TableColumn<Supplier, String> column1 = new TableColumn<>("Supplier Name");
-        column1.setCellValueFactory(
-                e -> new SimpleStringProperty(e.getValue().getName()));
+            column1.setCellValueFactory(
+                    e -> new SimpleStringProperty(e.getValue().getName()));
         TableColumn<Supplier, String> column2 = new TableColumn<>("Supplier Address");
-        column2.setCellValueFactory(
-                e -> new SimpleStringProperty(e.getValue().getAddress()));
+            column2.setCellValueFactory(
+                    e -> new SimpleStringProperty(e.getValue().getAddress()));
 
         TableColumn<Supplier, String> column3 = new TableColumn<>("Supplier Province");
-        column3.setCellValueFactory(
-                e -> new SimpleStringProperty(e.getValue().getProvince()));
+            column3.setCellValueFactory(
+                    e -> new SimpleStringProperty(e.getValue().getProvince()));
 
         TableColumn<Supplier, String> column4 = new TableColumn<>("Supplier City");
-        column4.setCellValueFactory(
-                e -> new SimpleStringProperty(e.getValue().getCity()));
+            column4.setCellValueFactory(
+                    e -> new SimpleStringProperty(e.getValue().getCity()));
 
         TableColumn<Supplier, String> column5 = new TableColumn<>("Supplier Postal Code");
-        column5.setCellValueFactory(
-                e -> new SimpleStringProperty(e.getValue().getPostalcode()));
+            column5.setCellValueFactory(
+                    e -> new SimpleStringProperty(e.getValue().getPostalcode()));
 
         TableColumn<Supplier, String> column6 = new TableColumn<>("Supplier Contact");
-        column6.setCellValueFactory(
-                e -> new SimpleStringProperty(e.getValue().getPhone()));
+            column6.setCellValueFactory(
+                    e -> new SimpleStringProperty(e.getValue().getPhone()));
 
         TableColumn<Supplier, String> column7 = new TableColumn<>("Supplier Email");
-        column7.setCellValueFactory(
-                e -> new SimpleStringProperty(e.getValue().getEmail()));
+            column7.setCellValueFactory(
+                    e -> new SimpleStringProperty(e.getValue().getEmail()));
 
         tableView.getColumns().addAll(column1, column2, column3, column4, column5, column6);
         tableView.getItems().addAll(supplier.getSuppliers()); // select * from suppliers
 
         root.setCenter(tableView);
 
-    }
+        }
 
-    private static TableColumn<Supplier, String> getSupplierStringTableColumn(SupplierTable supplierTable) {
+        private static TableColumn<Supplier, String> getSupplierStringTableColumn(SupplierTable supplierTable) {
         TableColumn<Supplier, String> column1 = new TableColumn<>("Supplier Name");
         column1.setCellValueFactory(e->new SimpleStringProperty(e.getValue().getName()));
         return column1;
-    }
-
-    public  void refreshTable() {
-        SupplierTable supplier = SupplierTable.getInstance();
-        tableView.getItems().clear();
-        tableView.getItems().addAll(supplier.getSuppliers());
-    }
-
-    public static SupplierTab getInstance() {
-        if (instance == null) {
-            instance = new SupplierTab();
         }
-        return instance;
+
+        public  void refreshTable() {
+            SupplierTable supplier = SupplierTable.getInstance();
+            tableView.getItems().clear();
+            tableView.getItems().addAll(supplier.getSuppliers());
+        }
+
+        public static SupplierTab getInstance() {
+            if (instance == null) {
+                instance = new SupplierTab();
+            }
+            return instance;
+        }
+
+
     }
-
-
-}
