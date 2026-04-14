427532b349cf2e8f3d60fbac0f9ebafaf695bd7c
hc <hcharikhi@gmail.com>
2026-04-13 21:13:00 -0400

    Fix previous commit * Commented code in the wrong class

--- src/main/java/org/example/doggofetch/tabs/user/UpdateUser.java
+++ src/main/java/org/example/doggofetch/tabs/user/UpdateUser.java
@@ -54,80 +54,80 @@ public class UpdateUserTab extends Tab {
         User user = selectedUser;
         UserTable userTable = UserTable.getInstance();
 
-        // product name
-        Text name = new Text("Name: ");
-        TextField tfName = new TextField();
-        tfName.setText(String.valueOf(product.getName()));
-        gridPane.add(name, 0, 0);
-        gridPane.add(tfName, 1, 0);
-
-        // product sku
-        Text sku = new Text("SKU: ");
-        TextField tfSku = new TextField();
-        tfSku.setText(String.valueOf(product.getSku()));
-        gridPane.add(sku, 0, 1);
-        gridPane.add(tfSku, 1, 1);
-
-        // product Qty
-        Text qty = new Text("Quantity: ");
-        TextField tfQty = new TextField();
-        tfQty.setText(String.valueOf(product.getSku()));
-        gridPane.add(qty, 0, 2);
-        gridPane.add(tfQty, 1, 2);
-
-        // product location
-        Text location = new Text("Location: ");
-        TextField tfLocation = new TextField();
-        tfLocation.setText(String.valueOf(product.getName()));
-        gridPane.add(location, 0, 3);
-        gridPane.add(tfLocation, 1, 3);
-
-        // category id comboBox
-        Text category = new Text("Category: ");
-        ComboBox<Category> comboCategory = new ComboBox<>();
-        ArrayList<Category> allCategories = categoryTable.getAllCategory();
-        comboCategory.setItems(FXCollections.observableArrayList(allCategories));
-        comboCategory.getSelectionModel().select(find(allCategories, product.getId()));
-        gridPane.add(category, 0, 4);
-        gridPane.add(comboCategory, 1, 4);
-
-        // supplier id comboBox
-        Text supplier = new Text("Supplier: ");
-        ComboBox<Supplier> comboSupplier = new ComboBox<>();
-        ArrayList<Supplier> allSuppliers = supplierTable.getAllSuppliers();
-        comboSupplier.setItems(FXCollections.observableArrayList(allSuppliers));
-        comboSupplier.getSelectionModel().select(find(allSuppliers, product.getId()));
-        gridPane.add(name, 0, 5);
-        gridPane.add(comboSupplier, 1, 5);
-
-        Button updateButton = new Button(" Update Product ");
-        updateButton.setOnAction((e -> {
-            product.setCategory(comboCategory.getSelectionModel().getSelectedItem().getId());
-            product.setSupplier(comboSupplier.getSelectionModel().getSelectedItem().getId());
-            product.setName(tfName.getText());
-            product.setSku(Integer.parseInt(tfSku.getText()));
-            product.setQuantity(Integer.parseInt(tfQty.getText()));
-            product.setLocation(tfLocation.getText());
-
-            productTable.updateProduct(product);
-
-        }));
-        gridPane.add(updateButton, 1, 6);
-    } // end UpdateProductPane
-
-//    public static UpdateProductTab getInstance(){
-//        if(instance == null){
-//            instance = new UpdateProductTab();
-//        }
-//        return instance;
-//    } // end create instance call
-
-    public void refreshTable(){
-        System.out.println("Test refresh");
-//        ItemTable table = ItemTable.getInstance();
-//        tableView.getItems().clear();
-//        tableView.getItems().addAll(table.getPrettyItems());
-    }
-
-
-} // end updateProductTab class
+//        // product name
+//        Text name = new Text("Name: ");
+//        TextField tfName = new TextField();
+//        tfName.setText(String.valueOf(product.getName()));
+//        gridPane.add(name, 0, 0);
+//        gridPane.add(tfName, 1, 0);
+//
+//        // product sku
+//        Text sku = new Text("SKU: ");
+//        TextField tfSku = new TextField();
+//        tfSku.setText(String.valueOf(product.getSku()));
+//        gridPane.add(sku, 0, 1);
+//        gridPane.add(tfSku, 1, 1);
+//
+//        // product Qty
+//        Text qty = new Text("Quantity: ");
+//        TextField tfQty = new TextField();
+//        tfQty.setText(String.valueOf(product.getSku()));
+//        gridPane.add(qty, 0, 2);
+//        gridPane.add(tfQty, 1, 2);
+//
+//        // product location
+//        Text location = new Text("Location: ");
+//        TextField tfLocation = new TextField();
+//        tfLocation.setText(String.valueOf(product.getName()));
+//        gridPane.add(location, 0, 3);
+//        gridPane.add(tfLocation, 1, 3);
+//
+//        // category id comboBox
+//        Text category = new Text("Category: ");
+//        ComboBox<Category> comboCategory = new ComboBox<>();
+//        ArrayList<Category> allCategories = categoryTable.getAllCategory();
+//        comboCategory.setItems(FXCollections.observableArrayList(allCategories));
+//        comboCategory.getSelectionModel().select(find(allCategories, product.getId()));
+//        gridPane.add(category, 0, 4);
+//        gridPane.add(comboCategory, 1, 4);
+//
+//        // supplier id comboBox
+//        Text supplier = new Text("Supplier: ");
+//        ComboBox<Supplier> comboSupplier = new ComboBox<>();
+//        ArrayList<Supplier> allSuppliers = supplierTable.getAllSuppliers();
+//        comboSupplier.setItems(FXCollections.observableArrayList(allSuppliers));
+//        comboSupplier.getSelectionModel().select(find(allSuppliers, product.getId()));
+//        gridPane.add(name, 0, 5);
+//        gridPane.add(comboSupplier, 1, 5);
+//
+//        Button updateButton = new Button(" Update Product ");
+//        updateButton.setOnAction((e -> {
+//            product.setCategory(comboCategory.getSelectionModel().getSelectedItem().getId());
+//            product.setSupplier(comboSupplier.getSelectionModel().getSelectedItem().getId());
+//            product.setName(tfName.getText());
+//            product.setSku(Integer.parseInt(tfSku.getText()));
+//            product.setQuantity(Integer.parseInt(tfQty.getText()));
+//            product.setLocation(tfLocation.getText());
+//
+//            productTable.updateProduct(product);
+//
+//        }));
+//        gridPane.add(updateButton, 1, 6);
+//    } // end UpdateProductPane
+//
+////    public static UpdateProductTab getInstance(){
+////        if(instance == null){
+////            instance = new UpdateProductTab();
+////        }
+////        return instance;
+////    } // end create instance call
+//
+//    public void refreshTable(){
+//        System.out.println("Test refresh");
+////        ItemTable table = ItemTable.getInstance();
+////        tableView.getItems().clear();
+////        tableView.getItems().addAll(table.getPrettyItems());
+//    }
+//
+//
+//} // end updateProductTab class
