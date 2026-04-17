50a4e429897faa0238cd18deb21524f7b1f4e6b3
kitkatkoe <kn01@myscc.ca>
2026-04-17 14:40:33 -0400

    productStats bug fix / pojo category to not extend databaseItem

--- src/main/java/org/example/doggofetch/pojo/Category.java
+++ src/main/java/org/example/doggofetch/pojo/Category.java
@@ -1,18 +1,18 @@
 package org.example.doggofetch.pojo;
 
-public class Category extends DatabaseItem{
+public class Category{
 
     private int id;
     private String name;
 
     public Category(int id, String name){
-        super(id);
+        this.id = id;
         this.name = name;
     }
 
-//    public Category(String name){
-//        this.name = name;
-//    }
+    public Category(String name){
+        this.name = name;
+    }
 //
 //    public Category(){
 //
